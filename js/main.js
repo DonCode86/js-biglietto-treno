74859b953c6bf244e094323f98a67bac3e4e23ce
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-04-28 14:29:30 +0200

    programma completo

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 //quanti chilomentri vuoi percorrere?
+const userKm = Number(prompt("Quanti chilometri vuoi percorrere?"));
 //quanti anni hai?
+const userAge = Number(prompt("Quanti anni hai?"));
 //calcolo prezzo totale del viaggio (0.21€/km)
+let totalPrice = userKm * 0.21;
+console.log(totalPrice);
 //se età_utente<18 sconto del 20%
-//se età_utente>=65 sconto del 40%
+if ( userAge < 18 ) {
+    totalPrice = totalPrice - totalPrice * 0.20;
+    console.log(totalPrice.toFixed(2)+" "+ "€");
+    //se età_utente>=65 sconto del 40%
+} else if ( userAge >= 69 ) {
+    totalPrice = totalPrice - totalPrice * 0.40;
+    console.log(totalPrice.toFixed(2)+" "+ "€");
+}
 //prezzo finale (max 2 decimali)
