313dce062e842069a5ab28cb751741aabfceeacb
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-04-28 13:34:03 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+//quanti chilomentri vuoi percorrere?
+//quanti anni hai?
+//calcolo prezzo totale del viaggio (0.21€/km)
+//se età_utente<18 sconto del 20%
+//se età_utente>=65 sconto del 40%
+//prezzo finale (max 2 decimali)
