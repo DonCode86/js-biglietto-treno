2dee748d8d44c6bc0d6c7632d811fc0704d4c971
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-04-28 14:42:20 +0200

    sistemazioni finali

--- js/main.js
+++ js/main.js
@@ -8,11 +8,11 @@ console.log(totalPrice);
 //se userAge<18 sconto del 20%
 let TotalPriceWithDiscount;
 if ( userAge < 18 ) {
-    TotalPriceWithDiscount = totalPrice - totalPrice * 0.20;
-    console.log(TotalPriceWithDiscount.toFixed(2)+" "+ "€");
+    totalPriceWithDiscount = totalPrice - totalPrice * 0.20;
+    console.log(totalPriceWithDiscount.toFixed(2)+" "+ "€");
     //se userAge>=65 sconto del 40%
 } else if ( userAge >= 65 ) {
-    TotalPriceWithDiscount = totalPrice - totalPrice * 0.40;
-    console.log(TotalPriceWithDiscount.toFixed(2)+" "+ "€");
+    totalPriceWithDiscount = totalPrice - totalPrice * 0.40;
+    console.log(totalPriceWithDiscount.toFixed(2)+" "+ "€");
 }
 //prezzo finale (max 2 decimali)
