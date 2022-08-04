5e12f9c98f9c967b2cb0ca0a6519bca8072bc009
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-08-04 08:34:12 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -1,18 +1,17 @@
 //quanti chilomentri vuoi percorrere?
-const userKm = Number(prompt("Quanti chilometri vuoi percorrere?"));
+const userKm = Number(prompt("Quanti chilometri vuoi percorrere?"))
 //quanti anni hai?
-const userAge = Number(prompt("Quanti anni hai?"));
-//calcolo prezzo totale del viaggio (0.21€/km)
-let totalPrice = userKm * 0.21;
-console.log(totalPrice);
-//se userAge<18 sconto del 20%
-let totalPriceWithDiscount;
-if ( userAge < 18 ) {
-    totalPriceWithDiscount = totalPrice - totalPrice * 0.20;
-    console.log(totalPriceWithDiscount.toFixed(2)+" "+ "€");
+const userAge = Number(prompt("Quanti anni hai?"))
+if (!isNaN(userKm) && !isNaN(userAge)) {
+  //calcolo prezzo totale del viaggio (0.21€/km)
+  let totalPrice = userKm * 0.21
+  //se userAge<18 sconto del 20%
+  if (userAge < 18) {
+    totalPrice = totalPrice - totalPrice * 0.2
     //se userAge>=65 sconto del 40%
-} else if ( userAge >= 65 ) {
-    totalPriceWithDiscount = totalPrice - totalPrice * 0.40;
-    console.log(totalPriceWithDiscount.toFixed(2)+" "+ "€");
+  } else if (userAge >= 65) {
+    totalPrice = totalPrice - totalPrice * 0.4
+  }
+  console.log(`il prezzo del biglietto è: ${totalPrice.toFixed(2) + " " + "€"}`)
 }
 //prezzo finale (max 2 decimali)
